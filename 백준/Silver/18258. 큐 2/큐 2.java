f5f390da10119964a381d6ce25c174d1427546cd
이소연 <87513664+reed426@users.noreply.github.com>
2025-01-29 22:58:04 +0900

    [Silver IV] Title: 큐 2, Time: 1364 ms, Memory: 351164 KB -BaekjoonHub

---
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        int num = Integer.parseInt(br.readLine());
+
+        Deque<String> q = new ArrayDeque<>();
+
+        for(int i=0;i<num;i++){
+            String[] str = br.readLine().split(" ");
+            switch(str[0]){
+                case "push":
+                    q.add(str[1]);
+                    break;
+                case "pop":
+                    if(q.isEmpty()) {
+                        bw.write("-1\n");
+                    } else{
+                        bw.write(q.remove()+"\n");
+                    }
+                    break;
+                case "size":
+                    bw.write(q.size()+"\n");
+                    break;
+                case "empty":
+                    if(q.isEmpty()) bw.write("1\n");
+                    else bw.write("0\n");
+                    break;
+                case "front":
+                    if(q.isEmpty()) bw.write("-1\n");
+                    else bw.write(q.peekFirst()+"\n");
+                    break;
+                case "back":
+                    if(q.isEmpty()) bw.write("-1\n");
+                    else bw.write(q.peekLast()+"\n");
+                    break;
+            }
+        }
+        bw.flush();
+    
+    }
+}
